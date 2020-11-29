fcbc29c0fab4ad54a9377e16d0ca3d6c1d4cd509
Ziyang Li <liby99@icloud.com>
2020-11-29 14:26:08 -0500

    Fixed compile error

--- png_destroy_write_struct_test.c
+++ png_destroy_write_struct_test.c
@@ -17,8 +17,8 @@ void png_destroy_write_struct(struct png_struct **png_ptr, struct png_info_struc
 
 int test() {
   struct png_struct *png_ptr;
-  struct png_info_struct *png_info_ptr;
-  
+  struct png_info_struct *info_ptr;
+
   png_ptr = png_create_write_struct();
   if (png_ptr == NULL) return 2;
   info_ptr = png_create_info_struct(png_ptr);
@@ -26,6 +26,6 @@ int test() {
     png_destroy_write_struct(&png_ptr, NULL);
     return 2;
   }
-  
+
   png_destroy_write_struct(&png_ptr, NULL);
 }
