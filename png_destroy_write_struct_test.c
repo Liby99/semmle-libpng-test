f2c468e554c46739703ae7e1b525f35da0150241
Ziyang Li <liby99@seas.upenn.edu>
2020-11-29 14:19:01 -0500

    Create png_destroy_write_struct_test.c

---
+++ png_destroy_write_struct_test.c
@@ -0,0 +1,31 @@
+#define NULL 0
+
+struct png_struct {};
+struct png_info_struct {};
+
+struct png_struct *png_create_write_struct() {
+  return 0;
+}
+
+struct png_info_struct *png_create_info_struct(struct png_struct *png_ptr) {
+  return 0;
+}
+
+void png_destroy_write_struct(struct png_struct **png_ptr, struct png_info_struct **info_ptr) {
+  return;
+}
+
+int test() {
+  struct png_struct *png_ptr;
+  struct png_info_struct *png_info_ptr;
+  
+  png_ptr = png_create_write_struct();
+  if (png_ptr == NULL) return 2;
+  info_ptr = png_create_info_struct(png_ptr);
+  if (info_ptr == NULL) {
+    png_destroy_write_struct(&png_ptr, NULL);
+    return 2;
+  }
+  
+  png_destroy_write_struct(&png_ptr, NULL);
+}
